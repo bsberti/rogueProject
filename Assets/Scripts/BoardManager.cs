086b44d6db3b6dd60786b7a96176fb74a291300a
bsberti <bsberti@gmail.com>
2017-09-06 10:32:07 -0300

    Local repository

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour 
+{
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5, 9);
+	public Count foodCount = new Count(1, 5);
+	public GameObject exit;
+	public GameObject[] floorTitles;
+    public GameObject[] foodTitles;
+    public GameObject[] wallTitles;
+    public GameObject[] enemyTitles;
+    public GameObject[] outerWallTitles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPosition = new List<Vector3>();
+
+    void InitialiaseList()
+    {
+        gridPosition.Clear();
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPosition.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTitles[Random.Range(0, floorTitles.Length)];
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                    toInstantiate = outerWallTitles[Random.Range(0, outerWallTitles.Length)];
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPosition.Count);
+        Vector3 randomPosition = gridPosition[randomIndex];
+        gridPosition.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    void LayoutObjectAtRandom(GameObject[] titleArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject titleChoise = titleArray[Random.Range(0, titleArray.Length)];
+            Instantiate(titleChoise, randomPosition, Quaternion.identity);
+        }
+    }
+
+	public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitialiaseList();
+        LayoutObjectAtRandom(wallTitles, wallCount.minimum, wallCount.maximum);
+        LayoutObjectAtRandom(foodTitles, foodCount.minimum, foodCount.maximum);
+        int enemyCount = (int)Math.Log(level, 2f);
+        LayoutObjectAtRandom(enemyTitles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected virtual void AttemptMove<T>(int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+        //Square magnitude is used instead of magnitude because it's computationally cheaper.
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        //While that distance is greater than a very small amount (Epsilon, almost zero):
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            //Find a new position proportionally closer to the end, based on the moveTime
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+
+            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+            rb2D.MovePosition(newPostion);
+
+            //Recalculate the remaining distance after moving.
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
+            yield return null;
+        }
+    }
+
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
+}
